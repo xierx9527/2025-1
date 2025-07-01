562eb3aaa060e42f958286f98977aa66239ea877
xierx9527 <1911696354@qq.com>
2025-07-01 16:37:21 +0800

    Add files via upload

---
+++ muxi7.1/front/app.js
@@ -0,0 +1,11 @@
+import Gallery from './Gallery';
+import { Profile } from './Profile';
+
+export default function App() {
+    return (
+        <div>
+            <Profile />
+            <Gallery />
+        </div>
+    );
+}
